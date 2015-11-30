c83e56d6785848c3014f23f178188c83fe484af8
715sunny <715xiaoyanzi@gmail.com>
2015-11-30 16:23:17 -0500

    service

--- app/src/main/java/com/example/sunny/ourpedometer/MainActivity.java
+++ app/src/main/java/com/example/sunny/ourpedometer/MainActivity.java
@@ -1,13 +1,70 @@
 package com.example.sunny.ourpedometer;
 
-import android.support.v7.app.AppCompatActivity;
+
+
+
+import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
+import android.widget.TextView;
+public class MainActivity extends Activity implements SensorEventListener
+{
+    private Handler mHandler = new Handler() {
+        @Override public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case STEPS_MSG:
+                    mStepValue = (int)msg.arg1;
+                    mStepValueView.setText("" + mStepValue);
+                    break;
+                case PACE_MSG:
+                    mPaceValue = msg.arg1;
+                    if (mPaceValue <= 0) {
+                        mPaceValueView.setText("0");
+                    }
+                    else {
+                        mPaceValueView.setText("" + (int)mPaceValue);
+                    }
+                    break;
+                case DISTANCE_MSG:
+                    mDistanceValue = ((int)msg.arg1)/1000f;
+                    if (mDistanceValue <= 0) {
+                        mDistanceValueView.setText("0");
+                    }
+                    else {
+                        mDistanceValueView.setText(
+                                ("" + (mDistanceValue + 0.000001f)).substring(0, 5)
+                        );
+                    }
+                    break;
+                case SPEED_MSG:
+                    mSpeedValue = ((int)msg.arg1)/1000f;
+                    if (mSpeedValue <= 0) {
+                        mSpeedValueView.setText("0");
+                    }
+                    else {
+                        mSpeedValueView.setText(
+                                ("" + (mSpeedValue + 0.000001f)).substring(0, 4)
+                        );
+                    }
+                    break;
+                case CALORIES_MSG:
+                    mCaloriesValue = msg.arg1;
+                    if (mCaloriesValue <= 0) {
+                        mCaloriesValueView.setText("0");
+                    }
+                    else {
+                        mCaloriesValueView.setText("" + (int)mCaloriesValue);
+                    }
+                    break;
+                default:
+                    super.handleMessage(msg);
+            }
+        }
 
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+    };
